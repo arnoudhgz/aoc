df32396a2112f450dae275075b89ac85edf30b70
Arnoud Beekman <a.beekman@youweagency.com>
2024-12-10 20:59:32 +0100

    Day 2: part 2

---
+++ 2024/2/part2.py
@@ -0,0 +1,58 @@
+from arg_utils import get_file_name
+from typeguard import typechecked
+
+# Use the utility function to get the file name
+file_name = get_file_name(__file__)
+
+@typechecked
+def is_valid(value: int, previous_value: int) -> bool:
+    distance = abs(value - previous_value);
+
+    return distance > 0 and distance < 4
+
+
+def is_valid_line(values: enumerate) -> bool:
+        increasing = False
+        decreasing = False
+
+        # Loop through values
+        for index, value in enumerate(values):
+            if index == 0:
+                continue
+
+            previous_value = int(values[index - 1])
+            new_value = int(value)
+
+            if new_value < previous_value:
+                if increasing:
+                    return False
+                decreasing = True
+
+            if new_value > previous_value:
+                if decreasing:
+                    return False
+                increasing = True
+
+            if not is_valid(new_value, previous_value):
+                return False
+            
+            previous_value = new_value
+
+        return True
+
+
+
+line_count = 0  # Initialize a counter
+
+with open(file_name, 'r') as file:
+    for line in file:
+        values = line.split()
+        for index, value in enumerate(values):
+            copy = values.copy()
+            copy.pop(index)
+            
+            if is_valid_line(copy):
+                line_count += 1
+                break
+
+print("Amount of safe lines:", line_count)
